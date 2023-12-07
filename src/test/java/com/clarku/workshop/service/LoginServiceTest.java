10d4f59dab49349d74dd21d2f4c39a404e667555
venkata-sai-reddy <47302950+venkata-sai-reddy@users.noreply.github.com>
2023-12-07 16:08:15 -0500

    Added Junit test cases

--- src/test/java/com/clarku/workshop/service/LoginServiceTest.java
+++ src/test/java/com/clarku/workshop/service/LoginServiceTest.java
@@ -1,7 +1,11 @@
 package com.clarku.workshop.service;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,10 +18,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.clarku.workshop.exception.GlobalException;
 import com.clarku.workshop.exception.LoginException;
+import com.clarku.workshop.repository.ISkillsRepo;
 import com.clarku.workshop.repository.LoginRepositoryImpl;
 import com.clarku.workshop.repository.UserRepositoryImpl;
 import com.clarku.workshop.utils.Secure;
 import com.clarku.workshop.vo.LoginVO;
+import com.clarku.workshop.vo.SignUpVO;
+import com.clarku.workshop.vo.SkillVO;
 import com.clarku.workshop.vo.UserVO;
 
 @RunWith(SpringRunner.class)
@@ -34,6 +41,9 @@ public class LoginServiceTest {
 
 	@Mock
 	UserRepositoryImpl userRepo;
+
+	@Mock
+	ISkillsRepo skillRepo;
 
 	@Mock
 	LoginVO loginDetails;
@@ -109,6 +119,14 @@ public class LoginServiceTest {
 		loginService.signIn(loginDetails);
 	}
 
+	@Test(expected = LoginException.class)
+	public void testSignIn_InActiveException() throws GlobalException, LoginException {
+		Mockito.when(loginRepo.retrieveUserLogin(Mockito.anyString())).thenReturn(loginDetails);
+		loginDetails.setIsActive(false);
+		Mockito.when(secure.getEncrypted(Mockito.anyString())).thenReturn(loginDetails.getPassword());
+		loginService.signIn(loginDetails);
+	}
+
 	@Test
 	public void testUpdateLastLogin_Success() throws GlobalException, LoginException {
 		Mockito.doNothing().when(loginRepo).updateLastLogin(Mockito.anyInt());
@@ -120,6 +138,115 @@ public class LoginServiceTest {
 	public void testUpdateLastLogin_Exception() throws GlobalException, LoginException {
 		Mockito.doThrow(LoginException.class).when(loginRepo).updateLastLogin(Mockito.anyInt());
 		loginService.updateLastLogin(2);
+	}
+
+	@Test
+	public void testSignUp_Success() throws GlobalException, LoginException {
+		Mockito.when(loginRepo.getUserId(Mockito.anyString())).thenReturn(1);
+		Mockito.when(loginRepo.saveLoginDetails(Mockito.any())).thenReturn(true);
+		Mockito.when(loginRepo.isUserExists(Mockito.anyString())).thenReturn(false);
+		SignUpVO signUpDetails = new SignUpVO();
+		signUpDetails.setConfirmPassword("pass");
+		signUpDetails.setCreatePassword("pass");
+		signUpDetails.setEmailId("email");
+		Boolean isSignUp = loginService.signUpUser(signUpDetails );
+		assertTrue(isSignUp);
+	}
+
+	@Test(expected = GlobalException.class)
+	public void testSignUp_PassMismatch() throws GlobalException, LoginException {
+		Mockito.when(loginRepo.getUserId(Mockito.anyString())).thenReturn(1);
+		Mockito.when(loginRepo.saveLoginDetails(Mockito.any())).thenReturn(true);
+		Mockito.when(loginRepo.isUserExists(Mockito.anyString())).thenReturn(false);
+		SignUpVO signUpDetails = new SignUpVO();
+		signUpDetails.setConfirmPassword("pass");
+		signUpDetails.setCreatePassword("pas");
+		signUpDetails.setEmailId("email");
+		loginService.signUpUser(signUpDetails );
+	}
+
+	@Test(expected = GlobalException.class)
+	public void testSignUp_UserExists() throws GlobalException, LoginException {
+		Mockito.when(loginRepo.getUserId(Mockito.anyString())).thenReturn(1);
+		Mockito.when(loginRepo.saveLoginDetails(Mockito.any())).thenReturn(true);
+		Mockito.when(loginRepo.isUserExists(Mockito.anyString())).thenReturn(true);
+		SignUpVO signUpDetails = new SignUpVO();
+		signUpDetails.setEmailId("email");
+		loginService.signUpUser(signUpDetails );
+	}
+
+	@Test
+	public void testSignUp_InstructorSuccess() throws GlobalException, LoginException {
+		Mockito.when(loginRepo.getUserId(Mockito.anyString())).thenReturn(1);
+		Mockito.when(loginRepo.saveLoginDetails(Mockito.any())).thenReturn(true);
+		Mockito.when(loginRepo.isUserExists(Mockito.anyString())).thenReturn(false);
+		SignUpVO signUpDetails = new SignUpVO();
+		signUpDetails.setConfirmPassword("pass");
+		signUpDetails.setCreatePassword("pass");
+		signUpDetails.setEmailId("email");
+		List<SkillVO> skills = new ArrayList<>();
+		SkillVO skill = new SkillVO();
+		skill.setSkillId(1);
+		skill.setSkillName("name");
+		skill.setStatus("Approved");
+		SkillVO newSkill = new SkillVO();
+		newSkill.setSkillName("new");
+		skills.add(skill);
+		signUpDetails.setExistingSkills(skills);
+		List<SkillVO> newSkills = new ArrayList<>();
+		newSkills.add(newSkill);
+		signUpDetails.setNewSkills(newSkills);
+		Boolean isSignUp = loginService.signUpUser(signUpDetails );
+		assertTrue(isSignUp);
+	}
+
+	@Test
+	public void testCreateUser_Success() throws GlobalException, LoginException {
+		Mockito.when(loginRepo.saveLoginDetails(Mockito.any())).thenReturn(true);
+		Mockito.when(userRepo.saveSignUpUser(Mockito.any())).thenReturn(true);
+		Mockito.when(loginRepo.isUserExists(Mockito.anyString())).thenReturn(false);
+		SignUpVO signUpDetails = new SignUpVO();
+		signUpDetails.setEmailId("email");
+		Boolean isCreated = loginService.createUser(signUpDetails );
+		assertTrue(isCreated);
+	}
+
+	@Test(expected = GlobalException.class)
+	public void testCreateUser_UserExists() throws GlobalException, LoginException {
+		Mockito.when(loginRepo.isUserExists(Mockito.anyString())).thenReturn(true);
+		SignUpVO signUpDetails = new SignUpVO();
+		signUpDetails.setEmailId("email");
+		loginService.createUser(signUpDetails );
+	}
+
+	@Test
+	public void testCreateUser_SaveFail() throws GlobalException, LoginException {
+		Mockito.when(loginRepo.saveLoginDetails(Mockito.any())).thenReturn(false);
+		Mockito.when(userRepo.saveSignUpUser(Mockito.any())).thenReturn(false);
+		Mockito.when(loginRepo.isUserExists(Mockito.anyString())).thenReturn(false);
+		SignUpVO signUpDetails = new SignUpVO();
+		signUpDetails.setEmailId("email");
+		assertFalse(loginService.createUser(signUpDetails));
+	}
+
+	@Test
+	public void testCreateUser_LoginSaveFail() throws GlobalException, LoginException {
+		Mockito.when(loginRepo.saveLoginDetails(Mockito.any())).thenReturn(false);
+		Mockito.when(userRepo.saveSignUpUser(Mockito.any())).thenReturn(true);
+		Mockito.when(loginRepo.isUserExists(Mockito.anyString())).thenReturn(false);
+		SignUpVO signUpDetails = new SignUpVO();
+		signUpDetails.setEmailId("email");
+		assertFalse(loginService.createUser(signUpDetails));
+	}
+
+	@Test
+	public void testCreateUser_ProfSaveFail() throws GlobalException, LoginException {
+		Mockito.when(loginRepo.saveLoginDetails(Mockito.any())).thenReturn(true);
+		Mockito.when(userRepo.saveSignUpUser(Mockito.any())).thenReturn(false);
+		Mockito.when(loginRepo.isUserExists(Mockito.anyString())).thenReturn(false);
+		SignUpVO createUserDetails = new SignUpVO();
+		createUserDetails.setEmailId("email");
+		assertFalse(loginService.createUser(createUserDetails));
 	}
 
 }
--- src/test/java/com/clarku/workshop/service/NotificationServiceTest.java
+++ src/test/java/com/clarku/workshop/service/NotificationServiceTest.java
@@ -2,6 +2,10 @@ package com.clarku.workshop.service;
 
 import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,8 +17,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.clarku.workshop.exception.EmailException;
 import com.clarku.workshop.exception.GlobalException;
+import com.clarku.workshop.utils.Constants;
 import com.clarku.workshop.utils.EmailHelper;
+import com.clarku.workshop.vo.CustomMessageVO;
+import com.clarku.workshop.vo.RegisteredUserVO;
 import com.clarku.workshop.vo.SignUpVO;
+import com.clarku.workshop.vo.SkillVO;
+import com.clarku.workshop.vo.UserVO;
+import com.clarku.workshop.vo.WorkshopVO;
 
 @RunWith(SpringRunner.class)
 public class NotificationServiceTest {
@@ -28,6 +38,21 @@ public class NotificationServiceTest {
 	@Mock
 	SignUpVO signUpDetails;
 
+	@Mock
+	CustomMessageVO messageVO;
+
+	@Mock
+	RegisteredUserVO registeredUser;
+
+	@Mock
+	SkillVO skillVO;
+
+	@Mock
+	UserVO userVO;
+
+	@Mock
+	WorkshopVO workshopVO;
+
 	@Before
 	public void initMocks() {
 		MockitoAnnotations.openMocks(this);
@@ -35,6 +60,18 @@ public class NotificationServiceTest {
 		signUpDetails.setEmailId("user@gmail.com");
 		signUpDetails.setFirstName("firstName");
 		signUpDetails.setUserType("Student");
+		
+		userVO = new UserVO();
+		userVO.setFirstName("firstName");
+		userVO.setEmailId("email");
+		
+		workshopVO = new WorkshopVO();
+		workshopVO.setWorkshopName("name");
+		
+		messageVO = new CustomMessageVO();
+		messageVO.setSendTo(new ArrayList<>());
+		messageVO.setSubject("subject");
+		messageVO.setMessage("message");
 	}
 
 	@Test
@@ -74,4 +111,225 @@ public class NotificationServiceTest {
 		notificationService.sendSuccessResetPassEmail("username@user.com", "firstname", "Des#2102");
 	}
 
+	@Test
+	public void testSendWorkshopCreateSuccessEmail_Success() throws GlobalException, EmailException {
+		notificationService.sendWorkshopCreateSuccessEmail(workshopVO, userVO);
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void testSendWorkshopCreateSuccessEmail_EmailException() throws GlobalException, EmailException {
+		Mockito.doThrow(EmailException.class).when(email).sendEMail(Mockito.any());
+		notificationService.sendWorkshopCreateSuccessEmail(workshopVO, userVO);
+	}
+
+	@Test
+	public void sendUpdatedWorkshopDetailsEmail_Success() throws GlobalException, EmailException {
+		notificationService.sendUpdatedWorkshopDetailsEmail(workshopVO, new ArrayList<>());
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void sendUpdatedWorkshopDetailsEmail_EmailException() throws GlobalException, EmailException {
+		Mockito.doThrow(EmailException.class).when(email).sendEMailMultipleBCC(Mockito.any());
+		notificationService.sendUpdatedWorkshopDetailsEmail(workshopVO, new ArrayList<>());
+	}
+
+	@Test
+	public void sendWorkshopUpdateSuccessEmail_Success() throws GlobalException, EmailException {
+		notificationService.sendWorkshopUpdateSuccessEmail(workshopVO, userVO);
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void sendWorkshopUpdateSuccessEmail_EmailException() throws GlobalException, EmailException {
+		Mockito.doThrow(EmailException.class).when(email).sendEMail(Mockito.any());
+		notificationService.sendWorkshopUpdateSuccessEmail(workshopVO, userVO);
+	}
+
+	@Test
+	public void sendWorkshopDeletedSuccessEmail_Success() throws GlobalException, EmailException {
+		notificationService.sendWorkshopDeletedSuccessEmail(workshopVO, userVO);
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void sendWorkshopDeletedSuccessEmail_EmailException() throws GlobalException, EmailException {
+		Mockito.doThrow(EmailException.class).when(email).sendEMail(Mockito.any());
+		notificationService.sendWorkshopDeletedSuccessEmail(workshopVO, userVO);
+	}
+
+	@Test
+	public void sendEnrollSuccessEmail_Success() throws GlobalException, EmailException {
+		notificationService.sendEnrollSuccessEmail(workshopVO, userVO);
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void sendEnrollSuccessEmail_EmailException() throws GlobalException, EmailException {
+		Mockito.doThrow(EmailException.class).when(email).sendEMail(Mockito.any());
+		notificationService.sendEnrollSuccessEmail(workshopVO, userVO);
+	}
+
+	@Test
+	public void sendWorkshopCancelledEmail_Success() throws GlobalException, EmailException {
+		notificationService.sendWorkshopCancelledEmail(workshopVO, new ArrayList<>());
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void sendWorkshopCancelledEmail_EmailException() throws GlobalException, EmailException {
+		Mockito.doThrow(EmailException.class).when(email).sendEMailMultipleBCC(Mockito.any());
+		notificationService.sendWorkshopCancelledEmail(workshopVO, new ArrayList<>());
+	}
+
+	@Test
+	public void sendRequestSuccessful_Success() throws GlobalException, EmailException {
+		notificationService.sendRequestSuccessful(userVO, "name");
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void sendRequestSuccessful_EmailException() throws GlobalException, EmailException {
+		Mockito.doThrow(EmailException.class).when(email).sendEMail(Mockito.any());
+		notificationService.sendRequestSuccessful(userVO, "name");
+	}
+
+	@Test
+	public void sendSkillRequestMail_Success() throws GlobalException, EmailException {
+		List<UserVO> usersList = new ArrayList<>();
+		UserVO user = new UserVO();
+		List<SkillVO> skillsList = new ArrayList<>();
+		SkillVO skill = new SkillVO();
+		skillsList.add(skill );
+		skill.setSkillName("name");
+		skill.setSkillId(1);
+		user.setSkills(skillsList);
+		user.setEmailId("emailId");
+		usersList.add(user);
+		notificationService.sendSkillRequestMail(usersList , 1, 2L);
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void sendSkillRequestMail_EmailException() throws GlobalException, EmailException {
+		List<UserVO> usersList = new ArrayList<>();
+		UserVO user = new UserVO();
+		List<SkillVO> skillsList = new ArrayList<>();
+		SkillVO skill = new SkillVO();
+		skillsList.add(skill );
+		skill.setSkillName("name");
+		skill.setSkillId(1);
+		user.setSkills(skillsList);
+		user.setEmailId("emailId");
+		usersList.add(user);
+		Mockito.doThrow(EmailException.class).when(email).sendEMailMultipleBCC(Mockito.any());
+		notificationService.sendSkillRequestMail(usersList, 1, 2L);
+	}
+
+	@Test
+	public void sendUnEnrollSuccessEmail_Success() throws GlobalException, EmailException {
+		notificationService.sendUnEnrollSuccessEmail(workshopVO, userVO);
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void sendUnEnrollSuccessEmail_EmailException() throws GlobalException, EmailException {
+		Mockito.doThrow(EmailException.class).when(email).sendEMail(Mockito.any());
+		notificationService.sendUnEnrollSuccessEmail(workshopVO, userVO);
+	}
+
+	@Test
+	public void sendPasswordChangeEmail_Success() throws GlobalException, EmailException {
+		notificationService.sendPasswordChangeEmail("email", "firstName");
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void sendPasswordChangeEmail_EmailException() throws GlobalException, EmailException {
+		Mockito.doThrow(EmailException.class).when(email).sendEMail(Mockito.any());
+		notificationService.sendPasswordChangeEmail("email", "firstName");
+	}
+
+	@Test
+	public void sendProfileUpdateEmail_Success() throws GlobalException, EmailException {
+		notificationService.sendProfileUpdateEmail("email", "firstName");
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void sendProfileUpdateEmail_EmailException() throws GlobalException, EmailException {
+		Mockito.doThrow(EmailException.class).when(email).sendEMail(Mockito.any());
+		notificationService.sendProfileUpdateEmail("email", "firstName");
+	}
+
+	@Test
+	public void sendCustomMessageToUsers_Success() throws GlobalException, EmailException {
+		notificationService.sendCustomMessageToUsers(userVO, messageVO);
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void sendCustomMessageToUsers_EmailException() throws GlobalException, EmailException {
+		Mockito.doThrow(EmailException.class).when(email).sendEMailMultipleBCC(Mockito.any());
+		notificationService.sendCustomMessageToUsers(userVO, messageVO);
+	}
+
+	@Test
+	public void sendSuccessCreateUserMail_Success() throws GlobalException, EmailException {
+		notificationService.sendSuccessCreateUserMail(signUpDetails);
+		assertTrue(true);
+	}
+
+	@Test
+	public void sendSuccessCreateUserMail_InstructorSuccess() throws GlobalException, EmailException {
+		signUpDetails.setUserType(Constants.INSTRUCTOR);
+		notificationService.sendSuccessCreateUserMail(signUpDetails);
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void sendSuccessCreateUserMail_EmailException() throws GlobalException, EmailException {
+		Mockito.doThrow(EmailException.class).when(email).sendEMail(Mockito.any());
+		notificationService.sendSuccessCreateUserMail(signUpDetails);
+	}
+
+	@Test
+	public void sendSuccessTemporaryPassword_Success() throws GlobalException, EmailException {
+		signUpDetails.setCreatePassword("pass");
+		notificationService.sendSuccessTemporaryPassword(signUpDetails);
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void sendSuccessTemporaryPassword_EmailException() throws GlobalException, EmailException {
+		signUpDetails.setCreatePassword("pass");
+		Mockito.doThrow(EmailException.class).when(email).sendEMail(Mockito.any());
+		notificationService.sendSuccessTemporaryPassword(signUpDetails);
+	}
+
+	@Test
+	public void sendUpcomingWorkshopNotification_Success() throws GlobalException, EmailException {
+		workshopVO.setCreatedUser("userName");
+		workshopVO.setWorkshopDate("12/10/2023");
+		workshopVO.setVenue("Venue");
+		workshopVO.setStartTime(LocalDateTime.now());
+		workshopVO.setMeetingURL("url");
+		workshopVO.setRegisteredUsers(new ArrayList<>());
+		notificationService.sendUpcomingWorkshopNotification(workshopVO, 2);
+		assertTrue(true);
+	}
+
+	@Test(expected = EmailException.class)
+	public void sendUpcomingWorkshopNotification_EmailException() throws GlobalException, EmailException {
+		workshopVO.setCreatedUser("userName");
+		workshopVO.setWorkshopDate("12/10/2023");
+		workshopVO.setVenue("Venue");
+		workshopVO.setStartTime(LocalDateTime.now());
+		workshopVO.setMeetingURL("url");
+		workshopVO.setRegisteredUsers(new ArrayList<>());
+		Mockito.doThrow(EmailException.class).when(email).sendEMailMultipleBCC(Mockito.any());
+		notificationService.sendUpcomingWorkshopNotification(workshopVO, 2);
+	}
+	
 }
